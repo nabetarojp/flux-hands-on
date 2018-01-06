0c273df07076cec64d811702664ab10678db8391
RyoWatanabe <30409123+wataanaber@users.noreply.github.com>
2018-01-06 16:44:45 +0900

    AppModule

---
+++ app/src/debug/java/jp/co/andfactory/master/di/AppModule.kt
@@ -0,0 +1,18 @@
+package jp.co.andfactory.master.di
+
+import dagger.Module
+import dagger.Provides
+import jp.co.andfactory.master.AppLifecycleCallbacks
+import jp.co.andfactory.master.data.di.DataModule
+import javax.inject.Singleton
+
+/**
+ * Created by watanabe on 2018/01/06.
+ */
+@Module(includes = [DataModule::class])
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideAppLifeCycleCallbacks(): AppLifecycleCallbacks = DebugAppLifecycleCallbacks()
+}
